92877b1199819a5af0642584a61213fac33702da
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-28 23:07:30 -0400

    The app bar and the first album are displaying on the page

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Text('Hello World',
-        style: TextStyle(color: Colors.red),
-        ),
-      ),
-
+     const MaterialApp(
+       home: AlbumPage(),
     ),
   );
+}
+
+class AlbumPage extends StatelessWidget {
+  const AlbumPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('What Beatles album should I listen to?'),
+      ),
+      body: AlbumChange(),
+    );
+  }
+}
+
+
+class AlbumChange extends StatefulWidget {
+
+  @override
+  State<AlbumChange> createState() => _AlbumChangeState();
+}
+
+class _AlbumChangeState extends State<AlbumChange> {
+
+  int albumNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+        child: Image.asset('images/$albumNumber.jpg')
+      ),
+    );
+  }
 }
