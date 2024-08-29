f3d624df442019243a0b88ec1633d01e3b6aa7a8
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-28 22:53:27 -0400

    Added inital files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Text('Hello World',
+        style: TextStyle(color: Colors.red),
+        ),
+      ),
+
+    ),
+  );
+}
